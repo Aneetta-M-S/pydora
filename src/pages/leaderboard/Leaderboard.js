a94004741a4354bf338fd4f7f1babcef288127fe
Shimil Shaji Abraham <shimilsabraham@gmail.com>
2023-04-09 10:10:02 +0530

    leaderboard page done

--- src/pages/leaderboard/Leaderboard.js
+++ src/pages/leaderboard/Leaderboard.js
@@ -1,7 +1,47 @@
 import "./Leaderboard.css"
+import { Toppers } from '../../components/LeaderboardStats/toppers'
 
 export const Leaderboard = () => {
     return (
-        <div>Leaderboard</div>
+        <>
+            <div className="main_container">
+                <div className="top"></div>
+                <div className="content leaderboard_content">
+                    <div className="leaderboard_container_title">
+                        Leaderboard
+                    </div>
+                    <div className="leaderboard_container">
+                        {
+                            // need to sort by position
+                            Toppers.map(user => {
+                                return (
+                                    <div 
+                                    className={user.position > 3 ? "leaderboard_main_card" : "leaderboard_main_card leaderboard_main_card_toppers"}
+                                    key={user.position} 
+                                    style={{backgroundColor: user.position > 3 ? "var(--remain)" : 
+                                                                user.position === 3 ? "var(--third)" :
+                                                                    user.position === 2 ? "var(--second)" : "var(--first)" }}>
+                                        <div className="pos">
+                                            {user.position}
+                                        </div>
+                                        <div className="name">{user.name}</div>
+                                        <div className="xp">{user.xp} XP</div>
+                                        <div className="dp">
+                                            <img src={user.dp} alt="" />
+                                        </div>
+                                    </div>
+
+                                )
+                            }
+                            )
+                        }
+                    </div >
+                </div>
+            </div>
+            <div className="right_container">
+                <div className="info"></div>
+                <div className="leaderboard_right_card"></div>
+            </div>
+        </>
     )
 }

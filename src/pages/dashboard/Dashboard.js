b2967ef81a20b1fd87018980f6b292ac05a58a0a
Shimil Shaji Abraham <shimilsabraham@gmail.com>
2023-04-10 09:34:27 +0530

    profile edit page done

--- src/pages/dashboard/Dashboard.js
+++ src/pages/dashboard/Dashboard.js
@@ -1,6 +1,10 @@
 import { MuiAccordion } from "../../components/MuiAccordion/MuiAccordion"
 import { LeaderboardStats } from "../../components/LeaderboardStats/Leaderboard_Stats"
 import TempXP from "../../assets/images/temp_xp.png"
+import PyLogo from "../../assets/images/pylogo.png"
+import Streak from "../../assets/images/streak.png"
+import Gem from "../../assets/images/gem.png"
+import Avatar from "../../assets/images/avatars/girl4.png"
 import "./Dashboard.css"
 
 export const Dashboard = () => {
@@ -13,10 +17,25 @@ export const Dashboard = () => {
                 </div>
             </div>
             <div className="right_container">
-                <div className="info"></div>
+                <div className="info">
+                    <div className="lang">
+                        <img src={PyLogo} alt="" />
+                    </div>
+                    <div className="streak_item">
+                        <img src={Streak} alt="" />
+                        <span>0</span>
+                    </div>
+                    <div className="gem_item">
+                        <img src={Gem} alt="" />
+                        <span>0</span>
+                    </div>
+                    <div className="dp_item">
+                        <img src={Avatar} alt="" />
+                    </div>
+                </div>
                 <LeaderboardStats />
                 <div className="chart">
-                    <img src={TempXP} alt=""/>
+                    <img src={TempXP} alt="" />
                 </div>
             </div>
         </>
--- src/pages/profile/Profile.js
+++ src/pages/profile/Profile.js
@@ -1,10 +1,80 @@
-import { Overlay } from "../../components/overlay/Overlay"
 import "./Profile.css"
+import Avatar from "../../assets/images/avatars/girl4.png"
 
 export const Profile = () => {
     return (
-        <div className="profile_page">
-            <Overlay />
-        </div>
+        <>
+            <div className="profile_edit_main_container">
+                <div className="top"></div>
+                <div className="content">
+                    <div className="profile_edit_title">Edit Profile</div>
+                    <div className="profile_edit_container">
+                        <div className="profile_edit_input">
+                            <span>Username</span>
+                            <input />
+                        </div>
+                        <div className="profile_edit_input">
+                            <span>Name</span>
+                            <input />
+                        </div>
+                        <div className="profile_edit_input">
+                            <span>Age</span>
+                            <input />
+                        </div>
+                        <div className="profile_edit_input">
+                            <span>About</span>
+                            <textarea />
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="profile_edit_right_container">
+                <div className="info"></div>
+                <div className="dp_option">
+                    <div className="dp_option_title">Set your profile pic</div>
+                    <div className="current_dp">
+                        <img src={Avatar} alt="" />
+                    </div>
+                    <div className="dp_option_set">
+                        <div className="dp_option_item selected_dp">
+                            <img src={Avatar} alt="" />
+                        </div>
+                        <div className="dp_option_item">
+                            <img src={Avatar} alt="" />
+                        </div>
+                        <div className="dp_option_item">
+                            <img src={Avatar} alt="" />
+                            </div>
+                        <div className="dp_option_item">
+                            <img src={Avatar} alt="" />
+                        </div>
+                        <div className="dp_option_item">
+                            <img src={Avatar} alt="" />
+                        </div>
+                        <div className="dp_option_item">
+                            <img src={Avatar} alt="" />
+                        </div>
+                        <div className="dp_option_item">
+                            <img src={Avatar} alt="" />
+                        </div>
+                        <div className="dp_option_item">
+                            <img src={Avatar} alt="" />
+                        </div>
+                        <div className="dp_option_item">
+                            <img src={Avatar} alt="" />
+                        </div>
+                        <div className="dp_option_item">
+                            <img src={Avatar} alt="" />
+                        </div>
+                        <div className="dp_option_item">
+                            <img src={Avatar} alt="" />
+                        </div>
+                        <div className="dp_option_item">
+                            <img src={Avatar} alt="" />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
     )
 }

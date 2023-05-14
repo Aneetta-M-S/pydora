b02d30bdfdca3ad80687fe4c56b592cdca9a8688
Shimil Shaji Abraham <shimilsabraham@gmail.com>
2023-05-14 22:23:14 +0530

    changed routing

--- src/App.js
+++ src/App.js
@@ -16,6 +16,11 @@ import { signOut } from 'firebase/auth';
 import { useEffect, useContext } from 'react';
 import { AuthContext } from './contexts/DetailsContext';
 
+import { Quiz1 } from './pages/quiz/level1/Quiz1'
+import { Quiz2 } from './pages/quiz/level1/Quiz2';
+import { Quiz3 } from './pages/quiz/level1/Quiz3';
+import { Quiz4 } from './pages/quiz/level1/Quiz4';
+
 function App() {
 
   const {user, logoutUser} = useContext(AuthContext)
@@ -37,12 +42,20 @@ function App() {
         <Routes>
           <Route index element={<Login />} />
           <Route element={<Navbar signUserOut={signUserOut} />} >
-            <Route path="/learn" element={<Dashboard />} />
-            <Route path="/leaderboard" element={<Leaderboard />} />
-            <Route path="/forum" element={<Forum />} />
-            <Route path="/profile">
-              <Route path="/profile" element={<Profile />} />
-              <Route path="/profile/edit" element={<EditProfile />} />
+            <Route path="learn" element={<Dashboard />} />
+            <Route path="leaderboard" element={<Leaderboard />} />
+            <Route path="forum" element={<Forum />} />
+            <Route path="profile">
+              <Route index element={<Profile />} />
+              <Route path="edit" element={<EditProfile />} />
+            </Route>
+          </Route>
+          <Route path="quiz">
+            <Route path='level1'>
+              <Route path='1' element={<Quiz1 />}/>
+              <Route path='2' element={<Quiz2 />}/>
+              <Route path='3' element={<Quiz3 />}/>
+              <Route path='4' element={<Quiz4 />}/>
             </Route>
           </Route>
 
